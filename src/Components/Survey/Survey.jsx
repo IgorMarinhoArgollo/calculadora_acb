fde15e33411759244b3cf68f09c1895d57ea5f5f
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2024-04-26 13:02:36 -0300

    feat: business rules

--- src/Components/Survey/Survey.jsx
+++ src/Components/Survey/Survey.jsx
@@ -2,24 +2,233 @@ import { useState } from 'react';
 import './Survey.scss';
 
 export default function Survey() {
-  const [disputeValue, setDisputeValue] = useState('');
+  const [disputeValue, setDisputeValue] = useState(0);
   const [disputeType, setDisputeType] = useState('');
-  const [numberOfArbitrators, setNumberOfArbitrators] = useState('');
+  const [numberOfArbitrators, setNumberOfArbitrators] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  const [adminitrationFee, setAdministrationFee] = useState(0);
+  const [registrationFee, setRegistrationFee] = useState(0);
+  const [arbitratorsFee, setArbitratorsFee] = useState(0);
+
+  const allQuestionsAnswered = () => {
+    return disputeValue !== '' && disputeType !== '' && numberOfArbitrators !== '';
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    await calculator();
+    console.log(arbitratorsFee);
+    console.log(typeof (arbitratorsFee));
+    setShowResult(true); 
+  };
+  
+  const calculator = async () => {
+    if (disputeValue > 0 && disputeValue <= 100000) {
+      setRegistrationFee(1500);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(6000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(6000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+
+    } 
+    
+    else if (disputeValue > 100000 && disputeValue <= 250000) {
+      setRegistrationFee(2000);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(10000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(10000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+
+    else if (disputeValue > 250000 && disputeValue <= 500000) {
+      setRegistrationFee(2500);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(15000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(15000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+
+    else if (disputeValue > 500000 && disputeValue <= 750000) {
+      setRegistrationFee(3000);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(19000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(19000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+      
+    else if (disputeValue > 750000 && disputeValue <= 1000000) {
+      setRegistrationFee(3500);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(23000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(23000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+
+    else if (disputeValue > 1000000 && disputeValue <= 2000000) {
+      setRegistrationFee(6500);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(30000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(30000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+
+    else if (disputeValue > 2000000 && disputeValue <= 5000000) {
+      setRegistrationFee(4500);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(32000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(32000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+
+    else if (disputeValue > 5000000 && disputeValue <= 10000000) {
+      setRegistrationFee(5000);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(38000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(38000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+
+    else if (disputeValue > 10000000 && disputeValue <= 20000000) {
+      setRegistrationFee(6000);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(45000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(45000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+
+    else if (disputeValue > 20000000 && disputeValue <= 50000000) {
+      setRegistrationFee(7000);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(52000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(52000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+      
+    else if (disputeValue > 50000000) {
+      setRegistrationFee(20000);
+      const adminFee = Math.min((0.02 * disputeValue), 180000);
+      setAdministrationFee(adminFee);
+
+      if (numberOfArbitrators === 1) {
+        setArbitratorsFee(() => parseFloat(130000 * 1.15));
+      } else {
+        setArbitratorsFee(() => parseFloat(130000 * numberOfArbitrators));
+      }
+
+      if (disputeType === 'exp') {
+        setAdministrationFee((adminitrationFee) => adminitrationFee * 0.7);
+        setArbitratorsFee((arbitratorsFee) => arbitratorsFee * 0.7);
+      }
+    }
+  };
 
   return (
     <div className='survey'>
       <h2>Para calcular as custas da sua arbitragem, preencha o questionário abaixo:</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="disputeValue" className='questions'>
           1. Qual o valor da demanda?
-          <input
-            type="number"
-            name="disputeValue"
-            id="disputeValue"
-            value={disputeValue}
-            onChange={(e) => setDisputeValue(e.target.value)}
-            step="any"
-          />
+          <span className="inputWrapper">
+            <input
+              type="number"
+              name="disputeValue"
+              id="disputeValue"
+              value={disputeValue}
+              onChange={(e) => setDisputeValue(parseFloat(e.target.value))}
+              step="any"
+            />
+          </span>
         </label>
 
         <label htmlFor="disputeType" className='questions'>
@@ -54,44 +263,56 @@ export default function Survey() {
           3. Qual o número de árbitros?
         </label>
         <div className='disputequestions'>
-          <label htmlFor="option1" className={`options ${numberOfArbitrators === '1' ? 'selected' : ''}`}>
+          <label htmlFor="option1" className={`options ${numberOfArbitrators === 1 ? 'selected' : ''}`}>
             <input
               type="radio"
               name="numberOfArbitrators"
               value="1"
               id="option1"
               checked={numberOfArbitrators === '1'}
-              onChange={() => setNumberOfArbitrators('1')}
+              onChange={() => setNumberOfArbitrators(parseInt(1))}
             />
             1
           </label>
-          <label htmlFor="option3" className={`options ${numberOfArbitrators === '3' ? 'selected' : ''}`}>
+          <label htmlFor="option3" className={`options ${numberOfArbitrators === 3 ? 'selected' : ''}`}>
             <input
               type="radio"
               name="numberOfArbitrators"
               value="3"
               id="option3"
               checked={numberOfArbitrators === '3'}
-              onChange={() => setNumberOfArbitrators('3')}
+              onChange={() => setNumberOfArbitrators(parseInt(3))}
             />
             3
           </label>
-          <label htmlFor="option5" className={`options ${numberOfArbitrators === '5' ? 'selected' : ''}`}>
+          <label htmlFor="option5" className={`options ${numberOfArbitrators === 5 ? 'selected' : ''}`}>
             <input
               type="radio"
               name="numberOfArbitrators"
               value="5"
               id="option5"
               checked={numberOfArbitrators === '5'}
-              onChange={() => setNumberOfArbitrators('5')}
+              onChange={() => setNumberOfArbitrators(parseInt(5))}
             />
             5
           </label>
         </div>
-        <p>{disputeValue}</p>
-        <p>{disputeType}</p>
-        <p>{numberOfArbitrators}</p>
+
+        <button
+          type="submit"
+          disabled={!allQuestionsAnswered()}
+          id='submitButton'>
+          Calcular
+        </button>
       </form>
+
+      {showResult && (
+        <div className="result">
+          <p>{registrationFee}</p>
+          <p>{adminitrationFee}</p>
+          <p>{arbitratorsFee}</p>
+        </div>
+      )}
     </div>
   )
 }
